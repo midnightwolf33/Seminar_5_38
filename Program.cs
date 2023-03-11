0a74ebf834c10bb2d4bb345e200bbb77ef6213f9
midnight33 <gra33@bk.ru>
2023-03-11 14:05:17 +0300

    Задача 38

---
+++ Program.cs
@@ -0,0 +1,44 @@
+﻿// Задача 38: Задайте массив вещественных чисел. Найдите разницу между максимальным и минимальным элементов массива.
+// [3 7 22 2 78] -> 76
+
+
+int[] TArray(int len, int minValue, int maxValue)
+{
+    int[] arr = new int[len];
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = new Random().Next(minValue, maxValue + 1);
+    }
+    return arr;
+}
+
+void PrintArr(int[] arr)
+{
+    Console.Write("[");
+    for (int i = 0; i < arr.Length - 1; i++)
+    {
+        Console.Write(arr[i] + ", ");
+    }
+    Console.WriteLine(arr[arr.Length - 1] + "]");
+}
+
+void PrintData(int res)
+{
+    Console.WriteLine(res);
+}
+
+int MaxMinSubtraction(int[] arr)
+{
+    int max = int.MinValue;
+    int min = int.MaxValue;
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (arr[i] > max) max = arr[i];
+        if (arr[i] < min) min = arr[i];
+    }
+    return max - min;
+}
+
+int[] BArr = TArray(10, 1, 10);
+PrintArr(BArr);
+PrintData(MaxMinSubtraction(BArr));
